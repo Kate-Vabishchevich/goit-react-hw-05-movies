9094dd9e0e961229c9d33eb7d9bbe3ab46eb8c3b
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-03-03 23:29:55 +0200

    movie details

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { getMovieDetails } from 'services/fetchAPI';
+import MovieCard from 'components/MovieCard/MovieCard';
 
 import css from './MovieDetails.module.css';
 
 const MovieDetails = () => {
-    const [movie, setMovis] = useState(null);
+    const [movie, setMovie] = useState(null);
+    const [error, setError] = useState(null);
+    const { movieId } = useParams();
+    const location = useLocation();
+
+    useEffect(() => {
+        getMovieDetails(movieId).then(response => {
+            setMovie({ ...response });
+        })
+            .catch(error => setError(error));
+    }, [movieId])
+
+    return (
+        <>
+            {movie && (
+                <>
+                    <MovieCard movie={movie} />
+                    <div>
+                        <h2>Additional information</h2>
+                        <ul className={css.list}>
+                            <li>Cast</li>
+                            <li>Reviews</li>
+                        </ul>
+                    </div>
+                </>
+            )}
+            <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+        </>
+    );
 }

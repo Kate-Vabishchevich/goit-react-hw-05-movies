390700b02495b21a0424871c08717e2b622b7703
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-03-11 00:13:24 +0200

    style for search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,36 +4,36 @@ import css from './SearchForm.module.css';
 import PropTypes from 'prop-types';
 
 const SearchForm = ({ onSubmit }) => {
-    const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
 
-    const onInputChange = e => setSearch(e.target.value);
-    // console.log('e:', e.target.value);
+  const onInputChange = e => setSearch(e.target.value);
+  // console.log('e:', e.target.value);
 
-    const onSubmitForm = e => {
-        e.preventDefault();
-        onSubmit(search.trim());
-        setSearch('');
-    }
+  const onSubmitForm = e => {
+    e.preventDefault();
+    onSubmit(search.trim());
+    setSearch('');
+  };
 
-    return (
-        <form className={css.form} onSubmit={onSubmitForm}>
-            <input
-                className={css.input}
-                onChange={onInputChange}
-                value={search}
-                name="search"
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search movies"
-            />
-            <button type="submit" className={css.button}>
-                <span className={css.button_label}></span>
-                <AiOutlineSearch size="24px" />
-            </button>
-        </form>
-    );
- }
+  return (
+    <form className={css.search_form} onSubmit={onSubmitForm}>
+      <input
+        className={css.search_input}
+        onChange={onInputChange}
+        value={search}
+        name="search"
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search movies"
+      />
+      <button type="submit" className={css.search_button}>
+        <span className={css.button_label}></span>
+        <AiOutlineSearch size="24px" />
+      </button>
+    </form>
+  );
+};
 
 export default SearchForm;
 
